fdc999a45921a406ca7bf4fafa437a36bbce16be
Mike Moss' Ubuntu Linux <michaelbmoss@gmail.com>
2019-03-03 14:15:45 -0800

    working onphase 2

--- urls.py
+++ urls.py
@@ -4,7 +4,7 @@ import views
 
 # In this example, we've separated out the views.py into a new file
 urlpatterns = [
-    path('index.html', views.index),
+    path('', views.index),
     path('bio.html', views.bio),
     path('contact.html', views.contact),
 ]
